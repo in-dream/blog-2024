405318d830f73bca89c400180c382f78c1ab5c29
chensu <r.92@qq.com>
2024-01-25 14:08:42 +0800

    更新 about-photo.js

--- static/js/about-photo.js
+++ static/js/about-photo.js
@@ -1,8 +1,8 @@
 //头部背景图随机显示
 var data = [
-  { text: '拍摄于杭州西湖', image: 'https://img.koobai.com/about.webp' },
-  { text: '拍摄于北京长城', image: 'https://img.koobai.com/about-beij.webp' },
-  { text: '拍摄于杭州千岛湖', image: 'https://img.koobai.com/about-qdh.webp' }
+  { text: '拍摄于杭州西湖', image: 'https://img.010316.xyz/usr/Obsidian/about.webp' },
+  { text: '拍摄于北京长城', image: 'https://img.010316.xyz/usr/Obsidian/about-beij.webp' },
+  { text: '拍摄于杭州千岛湖', image: 'https://img.010316.xyz/usr/Obsidian/about-qdh.webp' }
 ];
 
 var randomIndex = Math.floor(Math.random() * data.length);
